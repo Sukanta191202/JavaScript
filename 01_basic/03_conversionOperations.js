e0f9bf62feb3ff49282d9adc9c6940ad418aad1d
Sukanta191202 <sukantadua2002@gmail.com>
2026-02-19 19:55:32 +0530

    add conversion_Operations.js

---
+++ 01_basic/03_conversionOperations.js
@@ -0,0 +1,66 @@
+         //-------- Number conversion --------        
+// let score = 33
+// let score = "33"
+// let score = "33.44"
+// let score = "33abc"
+// let score = true
+// let score = false
+// let score = null
+// let score = undefined  
+let score = "Sukanta"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+
+let valueInNumber = Number(score);
+
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+//  "33" => 33
+// "33.44" => 33.44
+// "33abc" => Nan (Not a Number)
+// true => 1 ; false => 0;
+// null => 0
+// undefined => NaN
+// "Sukanta" => NaN
+
+
+//-------- String conversion --------
+// let someNubmer = 33
+// let someNumber = true
+// let someNumber = undefined
+let someNumber = 2+2
+
+let valueInString = String(someNumber)
+
+// console.log(valueInString);
+// console.log(typeof valueInString);
+
+// 33 => "33"
+// true => "true"
+// undefined => "undefined"
+// 2+2 => "4"
+
+
+// -------- Boolean conversion --------
+
+// let isLogedIn = 1
+// let isLogedIn = 0
+// let isLogedIn = "Sukanta"
+// let isLogedIn = ""
+// let isLogedIn = null
+let isLogedIn = undefined
+
+let valueInBoolean = Boolean(isLogedIn)
+
+console.log(valueInBoolean);
+console.log(typeof valueInBoolean);
+
+// 1 => true
+// 0 => false
+// "Sukanta" => true
+// "" => false
+// null => false
+// undefined => false
