0827b59aa9ed3074ab8200bef992e6cd2c5748f9
Sukanta191202 <sukantadua2002@gmail.com>
2026-02-20 12:23:30 +0530

    Changes command

--- 01_basic/04_comparison.js
+++ 01_basic/04_comparison.js
@@ -1,37 +1,37 @@
       // -------- Number to Number comparison --------
 
 
-     //console.log("Number Comparison =====>");
+     console.log("Number Comparison =====>");
     
-    //   console.log(2 > 1);   //true
-    //   console.log(2 < 1);   //false
-    //   console.log(2 == 1);  //false
-    //   console.log(2 >=1);   //true
-    //   console.log(2 <= 1);  //false
-    //   console.log(2 != 1);  //true
+      console.log(2 > 1);   //true
+      console.log(2 < 1);   //false
+      console.log(2 == 1);  //false
+      console.log(2 >=1);   //true
+      console.log(2 <= 1);  //false
+      console.log(2 != 1);  //true
 
 
         // -------- String to String comparison --------
 
-    //  console.log("String Comparison =====>");
+     console.log("String Comparison =====>");
       
-    //   console.log("a" > "b");  //false
-    //   console.log("a" < "b");  //true
-    //   console.log("a" == "b");  //false
-    //   console.log("a" >= "b");  //false
-    //   console.log("a" <= "b");  //true
-    //   console.log("a" != "b");  //true
+      console.log("a" > "b");  //false
+      console.log("a" < "b");  //true
+      console.log("a" == "b");  //false
+      console.log("a" >= "b");  //false
+      console.log("a" <= "b");  //true
+      console.log("a" != "b");  //true
 
 
 
   // -------- String to Number comparison --------
 
-    //   console.log("String to Number Comparison =====>");
+      console.log("String to Number Comparison =====>");
 
-    //   console.log("2" == 1);     // false
-    //   console.log("2" >= 1);     // true
-    //   console.log("02" >= 1);    // true
-    //   console.log(typeof("02"));  // string
+      console.log("2" == 1);     // false
+      console.log("2" >= 1);     // true
+      console.log("02" >= 1);    // true
+      console.log(typeof("02"));  // string
       
       
 // ---------  null and undefined comparison --------
@@ -42,10 +42,10 @@
      console.log(null < 0);   //false
      console.log(null >= 0);  //true   =>  that's why comparison check -----> Line (3)
      console.log(null <= 0);  //true  
-    //  console.log(null != 0);  //true
+     console.log(null != 0);  //true
 
-    //  console.log(null == 0);  //false  => that's why euality check
-    //console.log(null == "" );   //false
+     console.log(null == 0);  //false  => that's why euality check
+    console.log(null == "" );   //false
      
 /* Note : [The reason is that an equality check == and comparisons > < >= <= work differently.
            Comparisons convert null to a number, treating it as 0.
@@ -53,13 +53,13 @@
 */
 
 
-    //  console.log(undefined > 0);    //false 
-    //  console.log(undefined < 0);    //false
-    //  console.log(undefined == 0);   //false
+     console.log(undefined > 0);    //false 
+     console.log(undefined < 0);    //false
+     console.log(undefined == 0);   //false
 
-    //  console.log(null >= undefined);   //false
-    //  console.log(null <= undefined);   //false
-    //  console.log(null == undefined);   //true
+     console.log(null >= undefined);   //false
+     console.log(null <= undefined);   //false
+     console.log(null == undefined);   //true
 
     
 
@@ -68,8 +68,8 @@
 
         console.log("Strict Equality Check (===) =====>");
         
-         console.log( 2 === "2");  // false
-         console.log( "12" === 1 + "2");  // true 
+        console.log( 2 === "2");  // false
+        console.log( "12" === 1 + "2");  // true 
          
          
 
